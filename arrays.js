13c8ffd4fbc93b11ac95f7afd1d9b8b8d62fedc3
andiboshtrakaj <121944256+andiboshtrakaj@users.noreply.github.com>
2024-07-25 17:08:05 +0200

    Add files via upload

---
+++ arrays.js
@@ -0,0 +1,14 @@
+var guestLists = ["Andi","Harry","Jake","Logan","Prost","Charles"];
+
+var guestName = prompt("What is your name?");
+
+if(guestLists.includes(guestName)){
+    alert("Welcome to the party");
+}else{
+    alert("Sorry , your name is not on the list");
+};
+
+
+
+
+
