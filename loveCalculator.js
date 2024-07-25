7c3abec7b26ff5e3fce6c3edda9a0d269d6c560f
andiboshtrakaj <121944256+andiboshtrakaj@users.noreply.github.com>
2024-07-25 15:39:09 +0200

    Update loveCalculator.js

--- loveCalculator.js
+++ loveCalculator.js
@@ -1,18 +1,22 @@
-function loveCalc(){
-    var myname = prompt("Write Your Name") 
-    var yourname = prompt("Write his or her's name")
+function loveCalc() {
+    var myname = prompt("Write Your Name");
+    var yourname = prompt("Write his or her name");
     var randomNumber = Math.random();
-    randomNumber = randomNumber * 100 
-    randomNumber = Math.floor(randomNumber) + 1
+    randomNumber = randomNumber * 100;
+    randomNumber = Math.floor(randomNumber) + 1;
     console.log(randomNumber);
 
-    if (randomNumber > 70) {
-    alert("Your Love Percentage Is: " + randomNumber + "%" + "\nYou for sure love each other, do something");
-} else {
-    alert("Your Love Percentage Is: " + randomNumber + "%");
-}
-
-    
+    if (randomNumber >= 80 && randomNumber <= 90) {
+        alert("Your Love Percentage Is: " + randomNumber + "%" + "\nYou for sure love each other, do something");
+    } else if (randomNumber > 90 && randomNumber <= 94) {
+        alert("Your Love Percentage Is: " + randomNumber + "%" + "\nYou are soulmates");
+    } else if (randomNumber > 95) {
+        alert("Your Love Percentage Is: " + randomNumber + "%" + "\nYou are 1 in a billion");
+    } else if (randomNumber > 30 && randomNumber <= 60) {
+        alert("Your Love Percentage Is: " + randomNumber + "%");
+    } else if (randomNumber <= 30) {
+        alert("Your Love Percentage Is: " + randomNumber + "%" + "\nOuch!");
+    }
 }
 
 loveCalc();
