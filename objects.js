4aebac1d12fee0052e3ae0c5894fedca397ee77d
andiboshtrakaj <121944256+andiboshtrakaj@users.noreply.github.com>
2024-07-29 13:28:49 +0200

    Add files via upload

---
+++ objects.js
@@ -0,0 +1,8 @@
+function HouseKeeper(name, age, hasWorkPermit, languages){
+    this.name = name;
+    this.age = age;
+    this.hasWorkPermit = hasWorkPermit;
+    this.languages = languages;
+}
+
+    var HouseKeeper1 = new HouseKeeper("Andi", 17, true, ["Albanian", "English"]);
